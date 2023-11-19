060ee7f2cdf651b0625a8c90d5942b93047880bb
Ilya Lomonosov <lomonosov.iv@phystech.edu>
2023-11-19 17:09:14 +0400

    [DRAFT]

--- making_pizza/cli.py
+++ making_pizza/cli.py
@@ -1,5 +1,10 @@
+import time
+from random import randint
 import click
-from pizza import Margherita, Pepperoni, Hawaiian
+from making_pizza.pizza import Margherita, Pepperoni, Hawaiian
+from typing import Any
+
+from making_pizza.utils import log, log_with_template
 
 pizza_classes = {
     'margherita': Margherita,
@@ -13,26 +18,80 @@ pizza_emoji_dict = {
     'hawaiian': '🍍',
 }
 
+@log
+def bake(pizza: str) -> str:
+    """
+    Bake the specified pizza.
+
+    :param pizza: The name of the pizza to bake.
+    :type pizza: str
+    :return: A string indicating the pizza has been baked.
+    :rtype: str
+    """
+    time.sleep(randint(1, 3))
+    return f"Baked {pizza}!"
+
+@log_with_template("🚚 Delivered in {}s!")
+def deliver(pizza: str) -> str:
+    """
+    Deliver the specified pizza.
+
+    :param pizza: The name of the pizza to deliver.
+    :type pizza: str
+    :return: A string indicating the pizza has been delivered.
+    :rtype: str
+    """
+    time.sleep(randint(1, 3))
+    return f"Delivered {pizza}!"
+
+@log_with_template("🏠 Picked up in {}s!")
+def pickup(pizza: str) -> str:
+    """
+    Pick up the specified pizza.
+
+    :param pizza: The name of the pizza for pickup.
+    :type pizza: str
+    :return: A string indicating the pizza has been picked up.
+    :rtype: str
+    """
+    time.sleep(randint(1, 3))
+    return f"Picked up {pizza}!"
+
 @click.group()
-def cli():
-    """Pizza Ordering CLI"""
+def cli() -> None:
+    """
+    Pizza Ordering CLI.
+    """
     pass
 
 @cli.command()
 @click.argument('pizza_name', type=click.Choice(['margherita', 'pepperoni', 'hawaiian'], case_sensitive=False))
 @click.option('--size', type=click.Choice(['L', 'XL'], case_sensitive=False), default='L')
 @click.option('--delivery', is_flag=True, help='Include this flag to have the pizza delivered')
-def order(pizza_name, size, delivery):
-    """Order a pizza with optional delivery."""
+def order(pizza_name: str, size: str, delivery: bool) -> None:
+    """
+    Order a pizza with optional delivery.
+
+    :param pizza_name: The name of the pizza to order.
+    :param size: The size of the pizza.
+    :param delivery: Flag to indicate if the pizza needs to be delivered.
+    """
     pizza_class = pizza_classes[pizza_name.lower()]
     pizza = pizza_class(size)
-    delivery_message = " and will be delivered" if delivery else ""
+    delivery_message = " with delivery" if delivery else ""
     click.echo(f"Ordered a {size} {pizza_name.capitalize()} Pizza{delivery_message}!")
-    click.echo(', '.join(filter(None, pizza.dict().values())))
+    click.echo(f"Ingredients: {', '.join(filter(None, pizza.dict().values()))}")
+    click.echo(f"{bake(pizza_name.lower())}")
+    if delivery:
+        click.echo(deliver(pizza_name.lower()))
+    else:
+        click.echo(pickup(pizza_name.lower()))
 
 @cli.command()
-def menu():
-    """Show the pizza menu."""
+def menu() -> None:
+    """
+    Show the pizza menu.
+    """
     click.echo("Our pizza menu:")
     for name, cls in pizza_classes.items():
         pizza = cls('L')
--- making_pizza/pizza.py
+++ making_pizza/pizza.py
@@ -1,18 +1,39 @@
-from typing import Literal
+from typing import Dict, Literal
 
 class Pizza:
-    receipt = {}
+    """
+    Base class for Pizza, representing common properties and behaviors of pizzas.
+    """
 
-    def __init__(self, size: Literal['XL', 'L']):
+    receipt: Dict[str, str] = {}
+
+    def __init__(self, size: Literal['XL', 'L']) -> None:
+        """
+        Initialize the Pizza with a specific size.
+
+        :param size: Size of the pizza, either 'XL' or 'L'.
+        :type size: Literal['XL', 'L']
+        :raises ValueError: If the size is not 'XL' or 'L'.
+        """
         if size not in ['XL', 'L']:
             raise ValueError(f'Not such size: {size} only "XL" or "L"')
         self.size = size
 
-    def dict(self):
+    def dict(self) -> Dict[str, str]:
+        """
+        Returns the receipt of the pizza.
+
+        :return: A dictionary representing the receipt of the pizza.
+        :rtype: Dict[str, str]
+        """
         return type(self).receipt
 
 
 class Margherita(Pizza):
+    """
+    Margherita pizza class, inheriting from Pizza.
+    """
+
     receipt = {
        'ingredient_1': 'tomato sauce',
        'ingredient_2': 'mozzarella',
@@ -20,7 +41,12 @@ class Margherita(Pizza):
        'ingredient_4': None,
     }
 
+
 class Pepperoni(Pizza):
+    """
+    Pepperoni pizza class, inheriting from Pizza.
+    """
+
     receipt = {
        'ingredient_1': 'tomato sauce',
        'ingredient_2': 'mozzarella',
@@ -28,7 +54,12 @@ class Pepperoni(Pizza):
        'ingredient_4': None,
     }
 
+
 class Hawaiian(Pizza):
+    """
+    Hawaiian pizza class, inheriting from Pizza.
+    """
+
     receipt = {
        'ingredient_1': 'tomato sauce',
        'ingredient_2': 'mozzarella',
