2b4e9e9b0b5ab54b44d7fb08bcb5aa72471f254b
Ilya Lomonosov <lomonosov.iv@phystech.edu>
2023-11-19 18:06:48 +0400

    [UPDATE] Docstrings + annotations

--- tests/test.py
+++ tests/test.py
@@ -4,41 +4,75 @@ from making_pizza.cli import bake, cli, deliver, pickup
 
 
 @pytest.fixture
-def runner():
+def runner() -> CliRunner:
+    """
+    Pytest fixture to provide a CliRunner object for testing CLI commands.
+
+    :return: A CliRunner instance for testing.
+    :rtype: CliRunner
+    """
     return CliRunner()
 
 
-def test_bake():
+def test_bake() -> None:
+    """
+    Test the bake function to ensure it returns the correct response.
+    """
     pizza = "margherita"
     result = bake(pizza)
     assert "Baked margherita!" in result
 
 
-def test_deliver():
+def test_deliver() -> None:
+    """
+    Test the deliver function to ensure it returns the correct response.
+    """
     pizza = "pepperoni"
     result = deliver(pizza)
     assert "Delivered pepperoni!" in result
 
 
-def test_pickup():
+def test_pickup() -> None:
+    """
+    Test the pickup function to ensure it returns the correct response.
+    """
     pizza = "hawaiian"
     result = pickup(pizza)
     assert "Picked up hawaiian!" in result
 
 
-def test_order(runner):
+def test_order(runner: CliRunner) -> None:
+    """
+    Test the 'order' CLI command to ensure it processes orders correctly.
+
+    :param runner: A CliRunner instance for testing.
+    :type runner: CliRunner
+    """
     result = runner.invoke(cli, ["order", "margherita", "--size", "L"])
     assert "Ordered a L Margherita Pizza!" in result.output
     assert "Baked margherita!" in result.output
 
 
-def test_order_with_delivery(runner):
+def test_order_with_delivery(runner: CliRunner) -> None:
+    """
+    Test the 'order' CLI command with delivery option to ensure it
+    processes orders correctly.
+
+    :param runner: A CliRunner instance for testing.
+    :type runner: CliRunner
+    """
     result = runner.invoke(cli, ["order", "pepperoni", "--size", "XL", "--delivery"])
     assert "Ordered a XL Pepperoni Pizza with delivery!" in result.output
     assert "Delivered pepperoni!" in result.output
 
 
-def test_menu(runner):
+def test_menu(runner: CliRunner) -> None:
+    """
+    Test the 'menu' CLI command to ensure it displays the correct pizza menu.
+
+    :param runner: A CliRunner instance for testing.
+    :type runner: CliRunner
+    """
     result = runner.invoke(cli, ["menu"])
     assert "Our pizza menu:" in result.output
     assert "Margherita 🧀" in result.output
